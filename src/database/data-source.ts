376b2270493f10549afdf2bd4ef1879064cf8a79
arsalyy <arsalmuhammad70@gmail.com>
2025-10-02 11:51:03 +0500

    add fallback values for deployments

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.PGHOST,
+  host: process.env.PGHOST || 'db.ddtlulchmlyszupzehic.supabase.co',
   port: parseInt(process.env.PGPORT || '5432'),
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  username: process.env.PGUSER || 'postgres',
+  password: process.env.PGPASSWORD || '1234',
+  database: process.env.PGDATABASE || 'postgres',
   entities: [Task],
   migrations: ['src/database/migrations/*.ts'],
   synchronize: false,
